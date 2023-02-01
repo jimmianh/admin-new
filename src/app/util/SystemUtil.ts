7b702f925c0eab13f82f1b868115c13cb4313c15
NguyenHoang1303 <nguyenhsth2011012@fpt.edu.vn>
2023-02-01 10:54:47 +0700

    refactor code

--- src/app/util/SystemUtil.ts
+++ src/app/util/SystemUtil.ts
@@ -1,11 +1,7 @@
 export class SystemUtil {
 
   static getBaseUrl() {
-    return 'http://localhost:8080';
-  }
-
-  static getTokenTest() {
-    return "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsInBob25lIjoiMDk2OTQ1MTY5MSIsInByb2ZpbGVJZCI6MSwiZXhwIjoxNjcyODkxMTk5LCJpYXQiOjE2NzI4MDQ3OTksImF1dGhvcml0aWVzIjpbeyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dfQ.PRtrFN4rIQT5WdSCQfz3vkPPWzdKaNdJejMxLbKbgTg";
+    return 'https://herofund.up.railway.app';
   }
 
   static convertEnumToPaymentStatusList(enums: any) {
